de2795f35e9ddff504d673a637f7dcabd672582e
eomseungyeol <co6747@naver.com>
2021-07-30 23:13:44 +0900

    #navbar commit uncomplete

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,21 @@
 import React from'react';
 import {Nav, Navbar,Container} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import main from'./resource/main_icon.png';
 function Navigation(){
     return(
         <div className="Navigation">
               <Navbar bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="App.js">
+    <img
+      alt="main"
+      src="/resource/main_icon.png"
+      width="30"
+      height="30"
+      className="d-inline-block align-top"
+    />{' '}
+    MPF</Navbar.Brand>
     <Nav className="me-auto">
       <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
@@ -19,3 +28,4 @@ function Navigation(){
 
 }
 export default Navigation; 
+
