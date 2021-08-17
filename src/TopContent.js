c65f5fd804328c501c8ed86846bca5003fb57103
eomseungyeol <co6747@naver.com>
2021-08-17 21:52:47 +0900

    retry

--- src/TopContent.js
+++ src/TopContent.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
-import Button from 'react-bootstrap/Button'
+import {Container ,Button} from 'react-bootstrap'
+import './resource/1.png';
 function TopContent(){
     function handleScroll(){
+
         window.scroll({
             top:document.body.offsetHeight,
             left:0,
@@ -10,7 +12,11 @@ function TopContent(){
         });
     }
     return(
-        <div className="content">
+        <div className="content"
+       /* style={{
+            backgroundImage:
+            "url("+ require("resource/1.png").default +")",
+        }}*/>
             <h3>나만의 포토폴리오를 쉽게 만들어보세요 !</h3>
             <Button variant="secondary" size="lg" active onClick={handleScroll}>Start</Button>
         </div>
